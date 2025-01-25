43f5e93d91fc480fb622f26443450481e08f04af
SrishtiSharma89 <sharmasrishti487@gmail.com>
2025-01-25 17:33:28 +0545

    SelectFromEmployee.java

--- SelectFromEmployee.java
+++ SelectFromEmployee.java
@@ -1,22 +1,18 @@
 public class SelectFromEmployee {
     public static void main(String[] args) {
-        // Database credentials
+       
         String url = "jdbc:mysql://localhost:3306/employeedata"; // Replace 'localhost' with your server address
         String username = "root"; // Replace with your MySQL username
         String password = "PASSWORD"; // Replace with your MySQL password
 
-        // JDBC connection
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connected to the database!");
 
-            // Create a statement
             Statement statement = connection.createStatement();
 
-            // Execute a query
             String query = "SELECT * FROM employees";
             ResultSet resultSet = statement.executeQuery(query);
 
-            // Process the results
             System.out.println("ID | Name    | Age  | Salary");
             while (resultSet.next()) {
                 int id = resultSet.getInt("employee_id");
